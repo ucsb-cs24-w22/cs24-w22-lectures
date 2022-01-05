cb545871d627367e70052d08ca503236fb34857c
Diba Mirza <dibamirza@Dibas-MacBook-Pro.local>
2022-01-05 10:25:31 -0800

    lect01 with extra annotations

--- lect01/complex.cpp
+++ lect01/complex.cpp
@@ -1,27 +1,35 @@
 //complex.cpp
 // Review basics of classes
-// initialize objects: constructors
-// Complex number is of the form: a + j*b, j = sqrt(-1) 
-// Encapsulation
+// Example: Datatype to represent complex numbers (we could define this as a struct)
+// In general a complex number is of the form: a + j*b, where  a: real part  b: imaginary part
+// j = sqrt(-1)
+
+
 #include <iostream>
 using namespace std;
 class complex
 {
 private:
-    /* data */
-    // member variables
+    // member variables are typically private which means 
+    // they can only be accessed by member functions of the class
     double real;
     double imag;
 
 public:
+    // Member functions and variables in the public region can be accessed 
+    // by code outside the class (non-member fuunctions) 
+    
     complex(/* args */); //default constructor: initialize member variables of class
     ~complex(); //destructor : freeing up resources if needed
-    //getter functions
+    // getters (accessors): return value of member variables
     double getImag() const{
         return imag;
     }
+    // the const keyword at the end of the function means that
+    // the function getImag() cannot modify any of the member variables.
+    // Trying to modify the member variables, will result in a compiler error
     double getReal() const{
-        //real = 3;
+        //real = 3; this would result in a compiler error
         return real;
     
     }
@@ -47,7 +55,6 @@ complex::~complex()
 
 int main(int argc, char const *argv[])
 {
-    /* code */
     complex c1, c2;
     c1.print();
     //cout<<c1;
